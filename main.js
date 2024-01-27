9c3b2978a260a3bd85b8362a469b0093e995be9f
FRONTENDFORD4 <127622140+FRONTENDFORD4@users.noreply.github.com>
2024-01-27 15:38:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+
+
+function entrar(){
+
+try {
+    
+    let valor = document.getElementById('nome').value;
+    const locaStorage = localStorage.setItem('key', valor);
+
+    
+    window.location.href = "/primeiraTelaUser/";
+
+} catch (error) {
+    console.log('Error', error)
+} 
+
+}
+  
+let myItem = localStorage.getItem('key');
+
+if(myItem) {
+    let nomeUser = document.getElementById("nome-user");
+    nomeUser.textContent = myItem
+}
+
